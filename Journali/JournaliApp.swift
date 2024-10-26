ecbc8baa743edb8232ebd484141ef4a66348fafb
Wajdwael <wajdabdelhai@gmail.com>
2024-10-26 18:51:18 +0300

    Initial Commit

---
+++ Journali/JournaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliApp.swift
+//  Journali
+//
+//  Created by Wajd Wael on 23/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
