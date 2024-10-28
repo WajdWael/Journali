b950e9a8d6f2a875b721fb43afb239f422a83b36
Wajdwael <wajdabdelhai@gmail.com>
2024-10-28 09:25:24 +0300

    Add single note UI

--- Journali/Views/NotesView.swift
+++ Journali/Views/NotesView.swift
@@ -1,4 +1,4 @@
-//
+
 //  ContentView.swift
 //  Journali
 //
@@ -6,57 +6,67 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     @State private var searchText = ""
-    
-    let purpleBlackColor = "#141420"
     var body: some View {
-        ZStack {
-            // icons
-            HStack {
-                Spacer()
-                
-                Button(action: {
-                    print("Set up alarm")
-                }){
-                    Image(systemName: "line.3.horizontal.decrease")
-                        .padding()
-                        .background(Color("DarkGray"))
-                        .foregroundStyle(Color("LightPurple"))
-                        .font(.system(size: 22))
-                        .frame(width: 30, height: 30)
-                        .cornerRadius(50)
+        
+        NavigationStack {
+            List {
+                VStack {
+                    HStack {
+                        VStack {
+                            Text("Journali Title")
+                                .foregroundStyle(Color("LightPurple"))
+                                .font(.system(size: 24))
+                                .fontWeight(.semibold)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            
+                            Text("02/09/2024")
+                                .padding(.top, -6)
+                                .foregroundStyle(Color("Gray"))
+                                .font(.system(size: 14))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                        Spacer()
+                        
+                        Button(action: {
+                            print("Set up alarm")
+                        }) {
+                            Image(systemName: "bookmark")
+                                .font(.system(size: 24))
+                                .foregroundStyle(Color("LightPurple"))
+                        }
+                    }
+                    Text("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh viverra non semper suscipit posuere a pede.")
+                        .padding(.top, 26)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                 }
-                
-                Button(action: {
-                    print("Set up alarm")
-                }){
-                    Image(systemName: "plus")
-                        .padding()
-                        .background(Color("DarkGray"))
-                        .foregroundStyle(Color("LightPurple"))
-                        .font(.system(size: 22))
-                        .frame(width: 30, height: 30)
-                        .cornerRadius(50)
+
+                .cornerRadius(14)
+                .listRowSeparator(.hidden)
+                .swipeActions(edge: .leading) {
+                    Button {
+                        print("Edit button tapped")
+                    } label: {
+                        Label("", systemImage: "pencil")
+                    }
+                    .tint(Color("PurpleEditBtn"))
+                }
+                .swipeActions(edge: .trailing) {
+                    Button(role: .destructive) {
+                        print("Delete button tapped")
+                    } label: {
+                        Label("", systemImage: "trash")
+                    }
+                    .tint(Color("Red"))
                 }
             }
-            .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                        
+//            IF THERE IS NO NOTES
             
-            // header
-            HStack {
-                Text("Journali")
-                    .font(.system(size: 34, weight: .semibold))
-                    .foregroundStyle(.white)
-                    .padding(.top, 50)
-            }
-            .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-            
-            VStack(spacing: 20) {
-                
+            /*VStack(spacing: 20) {
                 Image("Journal")
+                
                 Text("Begin Your Journal")
                     .font(.system(size: 24, weight: .bold))
                     .foregroundStyle(Color("LightPurple"))
@@ -65,23 +75,54 @@ struct ContentView: View {
                     .font(.system(size: 18, weight: .light))
                     .foregroundStyle(.white)
                     .multilineTextAlignment(.center)
-                
+            }*/
+
+            
+            .navigationTitle("Journali")
+            .padding(.top, 10)
+
+            .toolbar {
+                ToolbarItem(){
+                    HStack(spacing: 5) {
+                        Button(action: {
+                            print("Set add new note function")
+                        }){
+                            Image(systemName: "plus")
+                                .padding()
+                                .background(Color("DarkGray"))
+                                .foregroundStyle(Color("LightPurple"))
+                                .font(.system(size: 18))
+                                .frame(width: 35, height: 35)
+                                .cornerRadius(50)
+                        }
+                        
+                        Menu {
+                            Button("Bookmark") {
+                                print("Order Now tapped")
+                            }
+                            Button("Journal Date") {
+                                print("Adjust Order tapped")
+                            }
+                        } label: {
+                            Image(systemName: "line.3.horizontal.decrease")
+                                .padding()
+                                .background(Color("DarkGray"))
+                                .foregroundStyle(Color("LightPurple"))
+                                .font(.system(size: 18))
+                                .frame(width: 35, height: 35)
+                                .cornerRadius(50)
+                        }
+                    }
+                }
             }
         }
-        // styles for the MAIN VIEW
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(.top)
         
-//        .background(
-//            LinearGradient(
-//                gradient: Gradient(colors: [
-//                    Color("DarkBlue"),
-//                    .black
-//                ]),
-//              startPoint: .top,
-//              endPoint: .bottom
-//            )
-//            .edgesIgnoringSafeArea(.all)
-//        )
+        // add functiontily to hide it when there are no notes
+        .searchable(text: $searchText, prompt: "Search Followers")
+        
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+      
     }
 }
 
